38e0315e61bfb4037410bc99b58e463488aa92ce
Ralph Bean <rbean@redhat.com>
2015-06-13 23:49:10 -0400

    Remove unused import.

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
-from codecs import open
 from os import path
 
 here = path.abspath(path.dirname(__file__))
