d647016ddf38adb8e10ca5b29c0bbf56691eb86d
Chaoyi Zha <summermontreal@gmail.com>
2015-09-18 23:24:18 -0400

    bind events rather than using onclick

--- mote/static/js/sresults.js
+++ mote/static/js/sresults.js
@@ -101,10 +101,10 @@ function showLogs(date_stamp) {
               link_prefix = window.meetbot_location + prefix_ending;
           }
           minutes.forEach(function (ele, ind, arr) {
-              minutes_markup += "<li>"+ ele +"<br /><a class='btn btn-info hidden-xs btn-xs' href='javascript:void();' onclick=\"openLogModal('"+ ele +"', '"+ prefix_ending +"');\">View</a>  <a target='_blank' class='btn btn-warning btn-xs' href='"+ link_prefix + ele +"'>Original</a> <a target='_blank' class='btn btn-success btn-xs' href='"+ prefix_ending + ele +"'>Permalink</a></li>";
+              minutes_markup += "<li>"+ ele +"<br /><a class='btn btn-info hidden-xs btn-xs openLogModal' data-ele=\""+ ele +"\" data-pfixending=\""+ prefix_ending +"\">View</a>  <a target='_blank' class='btn btn-warning btn-xs' href='"+ link_prefix + ele +"'>Original</a> <a target='_blank' class='btn btn-success btn-xs' href='"+ prefix_ending + ele +"'>Permalink</a></li>";
           });
           logs.forEach(function (ele, ind, arr) {
-              logs_markup += "<li>"+ ele +"<br /><a class='btn btn-info hidden-xs btn-xs' href='javascript:void();' onclick=\"openLogModal('"+ ele +"', '"+ prefix_ending +"');\">View Log</a>  <a target='_blank' class='btn btn-warning btn-xs' href='"+ link_prefix + ele +"'>Original</a> <a target='_blank' class='btn btn-success btn-xs' href='"+ prefix_ending + ele +"'>Permalink</a></li>";
+              logs_markup += "<li>"+ ele +"<br /><a class='btn btn-info hidden-xs btn-xs openLogModal' data-ele=\""+ ele +"\" data-pfixending=\""+ prefix_ending +"\">View Log</a>  <a target='_blank' class='btn btn-warning btn-xs' href='"+ link_prefix + ele +"'>Original</a> <a target='_blank' class='btn btn-success btn-xs' href='"+ prefix_ending + ele +"'>Permalink</a></li>";
           });
           minutes_markup += "</ul>";
           logs_markup += "</ul>";
@@ -119,5 +119,28 @@ $(function () {
     $(".meeting-day").click(function () {
         $(".meeting-day").removeClass("active");
         $(this).addClass("active");
+    });
+    $(".yearToggle").click(function () {
+        var year = $(this).data("year");
+        closeOthers();
+        openList(year);
+    });
+    $(".monthToggle").click(function () {
+        var yearMonth = $(this).data("month");
+        openList(yearMonth);
+    });
+    $(".dayToggle").click(function () {
+        var day = $(this).data("day");
+        showLogs(day);
+    });
+    $("#showLatestMeeting").click(function () {
+        var latestMeeting = $(this).data("latestmtg");
+        showLogs(latestMeeting);
+    });
+
+    $("body").delegate(".openLogModal", "click", function () {
+        var ele = $(this).data("ele");
+        var pfixending = $(this).data("pfixending");
+        openLogModal(ele, pfixending);
     });
 });
