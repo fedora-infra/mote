0a479a3061e87e64d264185064080621b42b72e1
Chaoyi Zha <summermontreal@gmail.com>
2015-10-24 21:34:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.1b1',
+    version='0.4.2',
 
     description="""møte is a Fedora IRC meeting log wrangler,
     allowing users to employ an easy-to-use web front to explore the Fedora Project's
