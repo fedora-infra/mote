cbe8956091ab2a108017a46019781e744947e889
Chaoyi Zha <summermontreal@gmail.com>
2016-01-21 18:23:23 -0500

    Change to 2015-2016

--- mote/static/js/sresults.js
+++ mote/static/js/sresults.js
@@ -1,6 +1,6 @@
 /*
 #
-# Copyright © 2016 Chaoyi Zha <cydrobolt@fedoraproject.org>
+# Copyright © 2015-2016 Chaoyi Zha <cydrobolt@fedoraproject.org>
 #
 # This copyrighted material is made available to anyone wishing to use,
 # modify, copy, or redistribute it subject to the terms and conditions
