fc8501ce4ed47dc4443796d1d7495ed6066e5888
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2021-09-27 23:24:09 +0530

    Added option to load up meeting from slug
    Filtered meeting metadata using regular expressions
    
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- modules/find.py
+++ modules/find.py
@@ -21,41 +21,86 @@
 
 import json
 import os
+import re
+import urllib.parse as ulpr
 
 directory_path = os.path.dirname("/srv/web/meetbot/")
+recognition_pattern = "(.*)[\-\.]([0-9]{4}-[0-9]{2}-[0-9]{2})-([0-9]{2}\.[0-9]{2})"
 
 
-def find_meetings_by_substring(search_string:str):
-	"""
-	Return list of meetings returned from search
-	"""
-	try:
-		meeting_dictionary = []
-		for root, dirs, files in os.walk(directory_path):
-			for file in files:
-				if search_string in file and not file.endswith(".tgz"):
-					location = "%s/%s" % (root, str(file))
-					if "/srv/web/meetbot/teams/" not in location:
-						location_list = location.replace("/srv/web/meetbot/", "").split("/")
-						channel_name, meeting_date, meeting_filename = location_list[0], location_list[1], location_list[2]
-						if ".log.html" in meeting_filename:
-							meeting_log_filename = meeting_filename
-							meeting_summary_filename = meeting_filename.replace(".log.html", ".html")
-							meeting_title = meeting_filename.replace(".log.html", "")
-							meeting_object = {
-								"meeting_topic": meeting_title,
-								"channel": channel_name,
-								"time": meeting_date,
-								"url": {
-									"logs": "https://meetbot-raw.fedoraproject.org/%s/%s/%s" % (channel_name, meeting_date, meeting_log_filename),
-									"summary": "https://meetbot-raw.fedoraproject.org/%s/%s/%s" % (channel_name, meeting_date, meeting_summary_filename),
-								}
-							}
-							meeting_dictionary.append(meeting_object)
-		return True, meeting_dictionary
-	except Exception as expt:
-		return False, {"exception": str(expt)}
+def find_meetings_by_substring(search_string: str):
+    """
+    Return list of meetings returned from search
+    """
+    try:
+        meeting_dictionary = []
+        for root, dirs, files in os.walk(directory_path):
+            for file in files:
+                if search_string in file and not file.endswith(".tgz"):
+                    location = "%s/%s" % (root, str(file))
+                    if "/srv/web/meetbot/teams/" not in location:
+                        location_list = location.replace("/srv/web/meetbot/", "").split(
+                            "/"
+                        )
+                        channel_name, meeting_date, meeting_filename = (
+                            location_list[0],
+                            location_list[1],
+                            location_list[2],
+                        )
+                        if ".log.html" in meeting_filename:
+                            meeting_log_filename = meeting_filename
+                            meeting_summary_filename = meeting_filename.replace(
+                                ".log.html", ".html"
+                            )
+                            meeting_title = re.search(
+                                recognition_pattern,
+                                meeting_filename.replace(".log.html", ""),
+                            )
+                            meeting_object = {
+                                "topic": meeting_title.group(1),
+                                "channel": channel_name,
+                                "date": meeting_date,
+                                "time": meeting_title.group(3),
+                                "url": {
+                                    "logs": "https://meetbot-raw.fedoraproject.org/%s/%s/%s"
+                                    % (
+                                        channel_name,
+                                        meeting_date,
+                                        meeting_log_filename,
+                                    ),
+                                    "summary": "https://meetbot-raw.fedoraproject.org/%s/%s/%s"
+                                    % (
+                                        channel_name,
+                                        meeting_date,
+                                        meeting_summary_filename,
+                                    ),
+                                },
+                                "slug": {
+                                    "logs": ulpr.quote(
+                                        "/%s/%s/%s"
+                                        % (
+                                            channel_name,
+                                            meeting_date,
+                                            meeting_log_filename,
+                                        ),
+                                        safe=":/?",
+                                    ),
+                                    "summary": ulpr.quote(
+                                        "/%s/%s/%s"
+                                        % (
+                                            channel_name,
+                                            meeting_date,
+                                            meeting_summary_filename,
+                                        ),
+                                        safe=":/?",
+                                    ),
+                                },
+                            }
+                            meeting_dictionary.append(meeting_object)
+        return True, meeting_dictionary
+    except Exception as expt:
+        return False, {"exception": str(expt)}
 
 
 if __name__ == "__main__":
-	print(json.dumps(find_meetings_by_substring(input()), indent=4))
+    print(json.dumps(find_meetings_by_substring(input()), indent=4))
