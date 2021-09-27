b147b85ad4a7f4dc678b00d02fb9b9dc597a200a
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2021-09-27 23:26:16 +0530

    Removed modal-based display of meeting details
    Made cosmetic changes to the search modal
    
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- static/jscn/fragment.js
+++ static/jscn/fragment.js
@@ -19,15 +19,33 @@
 ##########################################################################
 */
 
+async function initialize_search_modal() {
+    document.getElementById("find-body").innerHTML = `
+        <div id="find-init">
+            <div class="text-center mt-4">
+                <i class="display-1 fas fa-search"></i>
+            </div>
+            <div class="h4 head text-center mt-2 mb-2">
+                Search for conversations
+            </div>
+            <div class="small text-center mb-4">
+                Enter a substring to begin looking for the conversations
+            </div>
+        </div>
+        <div id="find-cont"></div>
+        <div id="none-find"></div>
+    `;
+    document.getElementById("findtext").value = "";
+}
+
 async function search_meetings() {
     let srchtext = document.getElementById("findtext").value;
-    document.getElementById("listfind-uols").innerHTML = "";
     document.getElementById("none-find").innerHTML = "";
     await $.getJSON("/fragedpt/", {
         "rqstdata": "srchmeet",
         "srchtext": srchtext
     }, function (data) {
-        if (JSON.stringify(data) === JSON.stringify([])) {
+        if (JSON.stringify(data) === JSON.stringify([]) || data.length === undefined) {
             document.getElementById("none-find").innerHTML = `
                 <div class="text-center mt-4">
                     <i class="display-1 fas fa-comment-slash"></i>
@@ -39,26 +57,31 @@ async function search_meetings() {
                     Please modify your search string to better fit what you are looking for
                 </div>
             `;
+            document.getElementById("find-cont").innerHTML = "";
+            document.getElementById("find-init").innerHTML = "";
         } else {
             document.getElementById("none-find").innerHTML = "";
+            document.getElementById("find-cont").innerHTML = `
+                <div class="text-center text-muted mt-1 mb-1">${data.length} conversation(s) found</div>
+                <ul class="list-group" id="listfind-uols"></ul>
+            `;
+            document.getElementById("find-init").innerHTML = "";
+            data = data.reverse();
             for (let indx in data) {
                 $("#listfind-uols").append(`
-                    <li class="list-group-item list-group-item-action" type="button">
-                        <div class="head h4">${data[indx]["meeting_topic"]}</div>
-                        <div class="fst-italic small">${data[indx]["time"]} on ${data[indx]["channel"]} channel</div>
-                        <div>
-                            <a class="btn btn-sm btn-outline-secondary"
-                               target="_blank"
-                               href="${data[indx]['url']['summary']}">
-                               View summary
-                            </a>
-                            <a class="btn btn-sm btn-outline-secondary"
-                               target="_blank"
-                               href="${data[indx]['url']['logs']}">
-                               View logs
-                            </a>
+                    <a class="list-group-item list-group-item-action" 
+                       type="button" 
+                       href="${data[indx]["slug"]["summary"]}" 
+                       target="_blank">
+                        <div class="head h4 mb-0 ellipsis">
+                            <span>${data[indx]["topic"]}</span>
                         </div>
-                    </li>
+                        <div class="fst-italic small text-muted mt-1">
+                            <i class="fas fa-clock"></i>&nbsp;${data[indx]["time"]}&nbsp;
+                            <i class="fas fa-calendar"></i>&nbsp;${data[indx]["date"]}&nbsp;
+                            <i class="fas fa-layer-group"></i>&nbsp;${data[indx]["channel"]}&nbsp;
+                        </div>
+                    </a>
                 `);
             }
         }
@@ -143,42 +166,22 @@ async function populate_meeting_list(channel, datetxt) {
         "channame": channel,
         "datename": datetxt
     }, function (data) {
-        const dataSorted = Object.entries(data)
-        dataSorted.forEach((element)=>{
-            const date_char_starts = element[0].search(/[1-9]/i);
-            const string_date = element[0].substr(date_char_starts, 16)
-            const string_date_with_commas = string_date.replace(/-|\./g, ",");
-            const array_date = string_date_with_commas.split(",")
-            const ready_to_format_date = array_date.map(Number);
-            const date = new Date(...ready_to_format_date);
-            element[2] = date
-        })
-        dataSorted.sort(function(a, b){ return b[2] - a[2] });
-        for (let i = 0; i < dataSorted.length; i++) {
-            const element = dataSorted[i];
-            const element_id = element[0] 
-            const logs_link = element[1].logs_link;
-            const summary_link = element[1].summary_link;
+        for (let indx in data) {
             $("#listmeet-uols").append(`
-            <li class="list-group-item list-group-item-action" 
-                type="button" 
-                data-bs-toggle="modal" 
-                data-bs-dismiss="modal" 
-                data-bs-target="#mainmode" 
-                onclick="render_meeting_logs_and_summary('${element_id}', '${logs_link}', '${summary_link}');">
-                <div class="head h4">
-                    ${element_id}
-                </div>
-                <div class="body small">
-                    <span class="fw-bold">Logs: </span>
-                    <a href="${logs_link}" target="_blank">${logs_link}</a>
-                </div>
-                <div class="body small">
-                    <span class="fw-bold">Summary: </span>
-                    <a href="${summary_link}" target="_blank">${summary_link}</a>
-                </div>
-            </li>
-        `);        
+                <a class="list-group-item list-group-item-action"
+                    type="button" 
+                    href="${data[indx]["slug"]["summary"]}" 
+                    target="_blank">
+                    <div class="head h4 mb-0 ellipsis">
+                        <span>${data[indx]["topic"]}</span>
+                    </div>
+                    <div class="fst-italic small text-muted mt-1">
+                        <i class="fas fa-clock"></i>&nbsp;${data[indx]["time"]}&nbsp;
+                        <i class="fas fa-calendar"></i>&nbsp;${data[indx]["date"]}&nbsp;
+                        <i class="fas fa-layer-group"></i>&nbsp;${data[indx]["channel"]}&nbsp;
+                    </div>
+                </a>
+            `);
         }
         document.getElementById("meethead").innerHTML = "Meetings on " + datetxt + " for " + channel;
         document.getElementById("meetfoot").innerHTML = "Pick a meeting of your choice";
@@ -188,10 +191,8 @@ async function populate_meeting_list(channel, datetxt) {
 async function populate_recent_meeting_list() {
     document.getElementById("listrcnt-daya-uols").innerHTML = "";
     document.getElementById("listrcnt-week-uols").innerHTML = "";
-    document.getElementById("listrcnt-mont-uols").innerHTML = "";
     document.getElementById("none-daya").innerHTML = "";
     document.getElementById("none-week").innerHTML = "";
-    document.getElementById("none-mont").innerHTML = "";
     document.getElementById("rcnthead").innerHTML = "Loading...";
     await $.getJSON("/fragedpt/", {
         "rqstdata": "rcntlsdy"
@@ -202,11 +203,6 @@ async function populate_recent_meeting_list() {
         "rqstdata": "rcntlswk"
     }, function (data) {
         populate_recent_meeting_on_dom(data, "week");
-    });
-    await $.getJSON("/fragedpt/", {
-        "rqstdata": "rcntlsmt"
-    }, function (data) {
-        populate_recent_meeting_on_dom(data, "mont");
     });
     document.getElementById("rcnthead").innerHTML = "Recent conversations";
 }
@@ -227,63 +223,19 @@ function populate_recent_meeting_on_dom(data, tabtitle) {
     } else {
         for (let indx in data) {
             $("#listrcnt-" + tabtitle + "-uols").append(`
-                <li class="list-group-item list-group-item-action" type="button">
-                    <div class="head h4">${data[indx]["meeting_topic"]}</div>
-                    <div class="fst-italic small">${data[indx]["time"]} on ${data[indx]["channel"]} channel</div>
-                    <div>
-                        <a class="btn btn-sm btn-outline-secondary" target="_blank" href="${data[indx]['url']['summary']}">View summary</a>
-                        <a class="btn btn-sm btn-outline-secondary" target="_blank" href="${data[indx]['url']['logs']}">View logs</a>
+                <a class="list-group-item list-group-item-action" 
+                   type="button" 
+                   href="${data[indx]["slug"]["summary"]}" 
+                   target="_blank">
+                    <div class="head h4 mb-0 ellipsis">
+                        <span>${data[indx]["topic"]}</span>
                     </div>
-                </li>
+                    <div class="fst-italic small text-muted mt-1">
+                        <i class="fas fa-clock"></i>&nbsp;${data[indx]["time"]}&nbsp;
+                        <i class="fas fa-layer-group"></i>&nbsp;${data[indx]["channel"]}&nbsp;
+                    </div>
+                </a>
             `);
         }
     }
-}
-
-async function render_meeting_logs_and_summary(name, logslink, summlink) {
-    document.getElementById("mainhead").innerHTML = "Loading...";
-    document.getElementById("mainfoot").innerHTML = `
-        <span class="spinner-border spinner-border-sm mt-2" role="status" aria-hidden="true"></span>
-    `;
-    document.getElementById("summ-cont").innerHTML = "";
-    document.getElementById("logs-cont").innerHTML = "";
-    document.getElementById("summ-qrcd").innerHTML = "";
-    document.getElementById("logs-qrcd").innerHTML = "";
-    await $.getJSON("/fragedpt/", {
-        "rqstdata": "obtntext",
-        "meetname": name,
-        "summlink": summlink,
-        "logslink": logslink
-    },function (data) {
-        new QRCode(
-            document.getElementById("summ-qrcd"), {
-                text: summlink,
-                width: 320,
-                height: 320,
-                colorDark:"#008080",
-                colorLight: "#ffffff",
-                correctLevel: QRCode.CorrectLevel.H,
-                logo: "/static/imgs/logoteal.png"
-        });
-        new QRCode(
-            document.getElementById("logs-qrcd"), {
-                text: logslink,
-                width: 320,
-                height: 320,
-                colorDark:"#008080",
-                colorLight: "#ffffff",
-                correctLevel: QRCode.CorrectLevel.H,
-                logo: "/static/imgs/logoteal.png"
-        });
-        document.getElementById("perm-summ-link").innerText = summlink;
-        document.getElementById("perm-summ-link").setAttribute("href", summlink);
-        document.getElementById("perm-logs-link").innerText = logslink;
-        document.getElementById("perm-logs-link").setAttribute("href", logslink);
-        document.getElementById("summ-butn").setAttribute("href", data["summary_slug"]);
-        document.getElementById("logs-butn").setAttribute("href", data["logs_slug"]);
-        document.getElementById("mainhead").innerText = name;
-        document.getElementById("summ-cont").innerHTML = data["summary_markup"];
-        document.getElementById("logs-cont").innerHTML = data["logs_markup"];
-        document.getElementById("mainfoot").innerHTML = "";
-    });
 }
