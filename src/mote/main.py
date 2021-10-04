b389c690ba96d2af80349e8cba3fc7929dd465ae
jaehoon hwang <jaehoon217@gmail.com>
2021-10-04 12:51:35 -0700

    File name change and css style changes and fix typo

--- src/mote/main.py
+++ src/mote/main.py
@@ -154,7 +154,7 @@ def mainfunc(portdata, netprotc):
 
 @main.errorhandler(404)
 def page_not_found(error):
-    return render_template("404.html"), 404
+    return render_template("e404page.html"), 404
 
 if __name__ == "__main__":
     mainfunc()
