3b5566d94b69c51c24e69445c7f340c121dd8eaa
darknao <naolwen@gmail.com>
2022-03-21 19:34:20 +0100

    retrieve log from old meetbot

--- mote/consume.py
+++ mote/consume.py
@@ -1,17 +1,22 @@
 import logging
 import os
 
+import rq
 from fedora_messaging.api import consume
 from fedora_messaging.config import conf
 from flask_socketio import SocketIO
+from redis import Redis
 
 conf.setup_logging()
 REDIS_URL = os.environ.get("REDIS_URL") or "redis://"
 socketio = SocketIO(message_queue=REDIS_URL)
+redis = Redis.from_url(REDIS_URL)
+task_queue = rq.Queue("tasks", connection=redis, default_timeout=60 * 10)
 
 
 def consume_fedora_messaging_msg(message):
     logging.info(message)
+    task_queue.enqueue("mote.tasks.process_new_meet", args=(message.body,))
     # Send client notification
     socketio.emit("show_toast", message.body)
 
--- mote/tasks.py
+++ mote/tasks.py
@@ -1,6 +1,8 @@
 import logging
+import os
+import urllib.request
 
-from mote import cache
+from mote import app, cache
 from mote.modules.find import get_meetings_files
 
 
@@ -8,3 +10,20 @@ def build_cache():
     logging.info("rebuilding cache")
     cache.delete_memoized(get_meetings_files)
     get_meetings_files()
+
+
+def process_new_meet(meet):
+    # reformat meetboot-raw url
+    # https://meetbot.fedoraproject.org/fedora-blocker-review/2022-03-21/f36-blocker-review.2022-03-21-16.01
+    basepath = meet["url"].replace(app.config["MEETBOT_URL"], "")
+    log_url = app.config["MEETBOT_RAW_URL"] + basepath + ".log.html"
+    log_path = app.config["MEETING_DIR"] + basepath + ".log.html"
+    smry_url = app.config["MEETBOT_RAW_URL"] + basepath + ".html"
+    smry_path = app.config["MEETING_DIR"] + basepath + ".html"
+    logging.info(f"Creating dir {os.path.dirname(smry_path)}")
+    os.makedirs(os.path.dirname(smry_path), exist_ok=True)
+    logging.info(f"downloading {smry_url}...")
+    urllib.request.urlretrieve(smry_url, smry_path)
+    logging.info(f"downloading {log_url}...")
+    urllib.request.urlretrieve(log_url, log_path)
+    # TODO: clear cache
